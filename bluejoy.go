0b3193a83aa42d71dba6414d86eddf733b6a487f
Taylor Monacelli <taylormonacelli@gmail.com>
2023-10-11 17:21:20 -0700

    Separate xdg app path to new package

--- bluejoy.go
+++ bluejoy.go
@@ -4,17 +4,14 @@ import (
 	"encoding/gob"
 	"log/slog"
 	"os"
-	"path/filepath"
 	"time"
 
-	"github.com/adrg/xdg"
 	gocache "github.com/patrickmn/go-cache"
+	"github.com/taylormonacelli/bluejoy/xdgapppath"
 )
 
-var configRelPath = "bluejoy/keys.db"
-
 func Main() int {
-	path, _ := genCachePath(configRelPath)
+	path, _ := xdgapppath.GenPath("bluejoy/keys.db")
 	slog.Debug("cache", "path", path)
 
 	cache1 := gocache.New(3*time.Minute, 4*time.Minute)
@@ -57,10 +54,10 @@ func Main() int {
 	}
 	defer file2.Close()
 
-	decoder := gob.NewDecoder(file2)
+	gobDecoder := gob.NewDecoder(file2)
 
 	var cacheMap map[string]gocache.Item
-	if err := decoder.Decode(&cacheMap); err != nil {
+	if err := gobDecoder.Decode(&cacheMap); err != nil {
 		slog.Debug("decode", "error", err.Error())
 		return 1
 	}
@@ -73,22 +70,4 @@ func Main() int {
 	slog.Debug("newCache", "found", found, "expires", expires, "now", time.Now(), "future", future, "reply", e.Pushes[0].URL)
 
 	return 0
-}
-
-func genCachePath(configRelPath string) (string, error) {
-	configFilePath, err := xdg.ConfigFile(configRelPath)
-	if err != nil {
-		return "", err
-	}
-
-	dirPerm := os.FileMode(0o700)
-
-	d := filepath.Dir(configFilePath)
-
-	if err := os.MkdirAll(d, dirPerm); err != nil {
-		slog.Error("cache", "mkdir", "error", err.Error())
-		return "", err
-	}
-
-	return configFilePath, nil
 }
