922a5bb86a3a30ad8d5f8a61dc360d72918050b0
Taylor Monacelli <taylormonacelli@gmail.com>
2023-10-11 17:28:08 -0700

    fiddleing

--- file.go
+++ file.go
@@ -9,10 +9,10 @@ func checkFileExists(path string) bool {
 	if _, err := os.Stat(path); err == nil {
 		return true
 	} else if os.IsNotExist(err) {
-		slog.Debug("checking cache file", "path", path, "exists", false)
+		slog.Debug("checking file existence", "path", path, "exists", false)
 		return false
 	} else {
-		slog.Error("checking for file", "path", path, "error", err.Error())
+		slog.Error("checking file type", "path", path, "error", err.Error())
 		panic(err)
 	}
 }
